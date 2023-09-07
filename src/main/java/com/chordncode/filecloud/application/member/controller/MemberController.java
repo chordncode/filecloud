036f241c8c580d3abc7620d98159a276f06c72d2
chordncode <chordncode@gmail.com>
2023-09-08 00:24:38 +0900

    file modified

--- src/main/java/com/chordncode/filecloud/application/member/controller/MemberController.java
+++ src/main/java/com/chordncode/filecloud/application/member/controller/MemberController.java
@@ -1,5 +1,6 @@
 package com.chordncode.filecloud.application.member.controller;
 
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.Cookie;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.chordncode.filecloud.application.member.service.MemberService;
 import com.chordncode.filecloud.config.jwt.JwtProvider;
+import com.chordncode.filecloud.config.mail.MailUtil;
 import com.chordncode.filecloud.config.util.ResultType;
 import com.chordncode.filecloud.data.dto.MemberDto;
 
@@ -22,6 +24,7 @@ public class MemberController {
 
     private final JwtProvider jwtProvider;
     private final MemberService memberService;
+    private final MailUtil mailUtil;
 
     @PostMapping("/signup")
     public ResultType signup(@RequestBody MemberDto memberDto) {
@@ -45,5 +48,10 @@ public class MemberController {
         return ResultType.FAILED;
     }
 
+    @PostMapping("/mail")
+    public ResultType mail(@RequestBody Map <String, String> param) {
+        mailUtil.sendMail("", "the mail subject", param.get("mailbody"));
+        return ResultType.SUCCESS;
+    }
 
 }
