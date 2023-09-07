185c5024762f71ee7270eaf45062debea2a2a9fb
chordncode <chordncode@gmail.com>
2023-09-07 23:30:34 +0900

    file modified

--- src/main/java/com/chordncode/filecloud/data/entity/MemberFileEntity.java
+++ src/main/java/com/chordncode/filecloud/data/entity/MemberFileEntity.java
@@ -2,7 +2,6 @@ package com.chordncode.filecloud.data.entity;
 
 import java.time.LocalDateTime;
 
-import javax.annotation.Generated;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
